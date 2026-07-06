bc85c5d9867df2c3723609d8172001dff8615364
Shrushti Pattar <srushti.apattar@gmail.com>
2026-07-06 23:15:58 +0530

    Added SystemVerilog struct example

---
+++ struct_example.sv
@@ -0,0 +1,19 @@
+module test;
+
+  // Anonymous structure representing a pixel
+  struct {
+    bit [7:0] r, g, b;
+    int color;
+  } pixel;
+
+  initial
+	begin
+	pixel.r     = 8'd25;
+    	pixel.g     = 8'd55;
+    	pixel.b     = 8'd11;
+    	pixel.color = 32'd811;
+
+   	$display("r=%d, g=%d, b=%d, color=%d", pixel.r, pixel.g, pixel.b, pixel.color);
+  	end
+
+endmodule
